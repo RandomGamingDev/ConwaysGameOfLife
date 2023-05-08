0d9bb0f455b0cb722aa969e308e8ce3eba9467f3
RandomGamingDev <83996185+RandomGamingDev@users.noreply.github.com>
2023-05-08 19:53:24 -0400

    Update keyboardInput.js

--- src/keyboardInput.js
+++ src/keyboardInput.js
@@ -11,14 +11,15 @@ function keyPressed() {
   switch (keyCode) {
     case 72: // h for help
       prompt(
-      `- Use p to pause & play
+      `- Use h for help
+       - Use p to pause & play
        - Use m to switch between place & erase mode
        - Use t to set the tick speed per frame (60fps)
        - Use c to declare cursor size
        - Use r to declare a new canvas with a new size
        - Use s to save the world as a png
        - Use l for loading an image to paste (p was already used lmao)
-       - Use esc to escape out of lay mode`)
+       - Use esc to escape out of image pasting mode`)
       break;
       
     case 80: // p for pause/play
