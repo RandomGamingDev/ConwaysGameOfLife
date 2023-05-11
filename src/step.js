0e430e87b9b539254d6586265be468a67bd65c0e
RandomGamingDev <ethanshiyi1123@gmail.com>
2023-05-11 18:09:27 -0400

    optimized a bit

--- src/step.js
+++ src/step.js
@@ -7,19 +7,10 @@ function step() {
   
   const stepShader = stepShaders[ncurBuf];
   
-  buffer.shader(stepShader);
   stepShader.setUniform("tex0", oBuffer);
-  stepShader.setUniform("aliveCellColor", aliveCellColor);
-  stepShader.setUniform("deadCellColor", deadCellColor);
-  stepShader.setUniform("screenSize", BufferDim().list);
+  
   buffer.background(0);
-  push();
-  {
-    buffer.noStroke();
-    buffer.rect(0, 0, buffer.width, buffer.height); 
-  }
-  pop();
-  buffer.resetShader();
+  buffer.rect(0, 0, buffer.width, buffer.height);
   
   curBuf = !curBuf;
 }
@@ -29,8 +20,30 @@ function steps() {
   if (nextRunTimer < 1)
       return;
       
-  for (let i = 0; i < Math.floor(nextRunTimer); i++)
-    step();
+  for (const i in buffers) {
+    const buffer = buffers[i];
+    const oBuffer = buffers[Number(!Boolean(i))]; 
+    
+    const stepShader = stepShaders[i];
+    
+    buffer.shader(stepShader);
+    stepShader.setUniform("aliveCellColor", aliveCellColor);
+    stepShader.setUniform("deadCellColor", deadCellColor);
+    stepShader.setUniform("screenSize", BufferDim().list); 
+  }
+      
+  push();
+  {
+    for (const buffer of buffers)
+      buffer.noStroke();
+  
+    for (let i = 0; i < Math.floor(nextRunTimer); i++)
+      step(); 
+  }
+  pop();
+  
+  for (const buffer of buffers)
+    buffer.resetShader();
   
   nextRunTimer %= 1;
 }
